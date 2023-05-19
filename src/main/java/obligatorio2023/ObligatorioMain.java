c683a0ab9edd600a052da16e175b62decec9504b
nicolassere <nicosgtr@gmail.com>
2023-05-19 15:54:11 -0300

    prueba

--- src/main/java/obligatorio2023/ObligatorioMain.java
+++ src/main/java/obligatorio2023/ObligatorioMain.java
@@ -6,5 +6,6 @@ public class ObligatorioMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Prueba II");
+        System.out.println("PEÑAROL");
     }
 }
