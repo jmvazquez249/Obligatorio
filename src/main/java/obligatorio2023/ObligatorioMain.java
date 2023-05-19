5cdc686aec77cda7186045e63b9ed530de943116
jmvazquez249 <jmvazquez@correo.um.edu.uy>
2023-05-19 15:52:48 -0300

    Hollaa

--- src/main/java/obligatorio2023/ObligatorioMain.java
+++ src/main/java/obligatorio2023/ObligatorioMain.java
@@ -4,6 +4,6 @@ package obligatorio2023;
 
 public class ObligatorioMain {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Holaaa");
     }
 }
