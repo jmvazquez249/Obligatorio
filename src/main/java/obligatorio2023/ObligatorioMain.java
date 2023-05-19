e23a40d6f3ed6eeb41db4549839ab0257e27f5a4
jmvazquez249 <jmvazquez@correo.um.edu.uy>
2023-05-19 15:52:16 -0300

    Sacar World

--- src/main/java/obligatorio2023/ObligatorioMain.java
+++ src/main/java/obligatorio2023/ObligatorioMain.java
@@ -4,6 +4,6 @@ package obligatorio2023;
 
 public class ObligatorioMain {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
