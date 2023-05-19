8a1e571b31a15480222304e208a58c89b22d646b
nicolassere <nicosgtr@gmail.com>
2023-05-19 15:45:36 -0300

    hello world ii

--- src/main/java/obligatorio2023/ObligatorioMain.java
+++ src/main/java/obligatorio2023/ObligatorioMain.java
@@ -5,6 +5,7 @@ package obligatorio2023;
 public class ObligatorioMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World II");
 
     }
 }
