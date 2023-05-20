a39ba1907e0ecd547be4279eacdc035c8048fb0a
jmvazquez249 <jmvazquez@correo.um.edu.uy>
2023-05-20 11:22:07 -0300

    CAMBIOS

--- src/main/java/obligatorio2023/TADs/MyLinkedList/MyList.java
+++ src/main/java/obligatorio2023/TADs/MyLinkedList/MyList.java
@@ -4,53 +4,14 @@ import obligatorio2023.Exception.IllegalIndexException;
 
 public interface MyList<T>
 {
-    /*
-    * Retorna verdadero si la lista no contiene elementos.
-    * */
-    public boolean esVacia();
-
-    /*
-    * Agreaga al principio de la lista. (Agrega en la cabeza o primero)
-    * */
-    public void agregar(T obj);
-
-    /*
-    * Agrega al final de la lista. (Agrega en la cola o último)
-    * */
-    public void agregarAlFinal(T obj);
-
-    /*
-    * Agrega en la posición indicada por el parámetro "index".
-    * Se lanza excepción si el índice es negativo o mayór a la última posición donde se puede agregar en la lista.
-    * */
-    public void agregar(T obj, int index) throws IllegalIndexException, IllegalIndexException;
-
-    /*
-    * Devuelve el largo de la lista. Si está vacía se devuelve 0
-    * */
-    public int largo();
-
-    /*
-    * Elimina un elemento de la lista en la posición "index" y devuelve el dato que se elimina.
-    * Si la posición index no corresponde a una posición válida de la lista, se devuelve null
-    * */
-    public T quitar(int index);
-
-    /*
-     * Devuelve el dato de la lista en la posición "index".
-     * Si la posición index no corresponde a una posición válida de la lista, se devuelve null
-     * */
-    public T elemento(int index);
-
-    /*
-    * La lista se queda sin elementos. La cabeza apunta a null y el largo es 0
-    * */
-    public void vaciar();
-
-    /*
-     * Emite por patnalla un toString de cada uno de los datos almacenados en los nodos con el siguiente formato:
-     * <#indice>: <to string del objeto>
-     * */
-    public void imprimirDatos();
+    boolean esVacia();
+    void agregar(T obj);
+    void agregarAlFinal(T obj);
+    void agregar(T obj, int index) throws IllegalIndexException;
+    int largo();
+    T quitar(int index);
+    T elemento(int index);
+    void vaciar();
+    void imprimirDatos();
 
 }
