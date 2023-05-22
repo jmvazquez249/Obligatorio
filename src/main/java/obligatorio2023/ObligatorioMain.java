2cd92c5dee2b14ab9e4ed5400660814199fa3719
jmvazquez249 <jmvazquez@correo.um.edu.uy>
2023-05-22 15:55:38 -0300

    Correccion Delete

--- src/main/java/obligatorio2023/ObligatorioMain.java
+++ src/main/java/obligatorio2023/ObligatorioMain.java
@@ -5,8 +5,5 @@ package obligatorio2023;
 public class ObligatorioMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("PEÑAROL");
-        System.out.println("confirmarx2");
-        
     }
 }
